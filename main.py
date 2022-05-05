dc608cd6d2a2ce7b5ec5dfffaff9934b080eb057
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2022-05-04 21:20:07 -0300

    Commit Inicial. Terminado programa

---
+++ main.py
@@ -0,0 +1,39 @@
+import datetime
+from datetime import timedelta
+
+lines = []
+
+with open('TAS.csv') as f:
+    # Read all lines on the file
+    linesRead = f.readlines()
+
+    # For each linea, remove the newline character and split the line into a list of strings
+    for line in linesRead:
+        line = line.rstrip()
+        line = line.split(';')
+        lines.append(line)
+
+# Print each element of the list in a new line
+for line in lines:
+    print(line)
+
+times = []
+
+last_time = datetime.datetime.now()
+
+times.append(last_time.strftime("%d/%m/%Y %H:%M:%S"))
+
+for line in lines:
+    for i in range(0, int(line[1])):
+        mins_sum = timedelta(minutes=(int(line[0])))
+        new_time = last_time + mins_sum
+        last_time += mins_sum
+        times.append(new_time.strftime("%d/%m/%Y %H:%M:%S"))
+
+for time in times:
+    print(time)
+
+# Write all the times to a new file
+with open('TAS.txt', 'w') as f:
+    for time in times:
+        f.write(time + '\n')
