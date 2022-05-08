ea12d5e06c0e97db62f2b55ec2133f28d4f3a377
Ignacio Baptista [Desktop] <ibaptista@frba.utn.edu.ar>
2022-05-08 17:06:27 -0300

    Terminado programa

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 import datetime
 from datetime import timedelta
 
-lines = []
 
-with open('TAS.csv') as f:
-    # Read all lines on the file
-    linesRead = f.readlines()
+def readFileAndGenerate(filename):
+    lines = []
 
-    # For each linea, remove the newline character and split the line into a list of strings
-    for line in linesRead:
-        line = line.rstrip()
-        line = line.split(';')
-        lines.append(line)
+    with open(filename + ".csv") as f:
+        # Read all lines on the file
+        linesRead = f.readlines()
 
-# Print each element of the list in a new line
-for line in lines:
-    print(line)
+        # For each linea, remove the newline character and split the line into a list of strings
+        for line in linesRead:
+            line = line.rstrip()
+            line = line.split(';')
+            lines.append(line)
 
-times = []
+    # Print each element of the list in a new line
+    # for line in lines:
+    #    print(line)
 
-last_time = datetime.datetime.now()
+    times = []
 
-times.append(last_time.strftime("%d/%m/%Y %H:%M:%S"))
+    last_time = datetime.datetime.now()
 
-for line in lines:
-    for i in range(0, int(line[1])):
-        mins_sum = timedelta(minutes=(int(line[0])))
-        new_time = last_time + mins_sum
-        last_time += mins_sum
-        times.append(new_time.strftime("%d/%m/%Y %H:%M:%S"))
+    times.append(last_time.strftime("%d/%m/%Y %H:%M:%S"))
 
-for time in times:
-    print(time)
+    for line in lines:
+        for i in range(0, int(line[1])):
+            mins_sum = timedelta(minutes=(int(line[0])))
+            new_time = last_time + mins_sum
+            last_time += mins_sum
+            times.append(new_time.strftime("%d/%m/%Y %H:%M:%S"))
 
-# Write all the times to a new file
-with open('TAS.txt', 'w') as f:
-    for time in times:
-        f.write(time + '\n')
+    # for time in times:
+    #    print(time)
+
+    # Write all the times to a new file
+    with open(filename + ".txt", 'w') as f:
+        for time in times:
+            f.write(time + '\n')
+
+    print("File '" + filename + ".txt' generated successfully")
+
+
+readFileAndGenerate("Generacíón de datos - IA - New")
